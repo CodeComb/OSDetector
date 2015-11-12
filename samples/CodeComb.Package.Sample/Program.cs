886dc4b8593eb3014258c36da2696d73d7fad7fe
あまみや ゆうこ <1@1234.sh>
2015-11-12 16:08:15 +0800

    Git clone

--- samples/CodeComb.Package.Sample/Program.cs
+++ samples/CodeComb.Package.Sample/Program.cs
@@ -10,6 +10,9 @@ namespace CodeComb.Package.Sample
         public static void Main(string[] args)
         {
             Console.WriteLine(OS.Current.ToString());
+            var result = GitClone.Clone("https://github.com/aspnet/Home", @"C:\Pool\test");
+            Console.WriteLine(result.Error);
+            Console.WriteLine(result.Output);
             Download.DownloadAndExtractAll("https://github.com/CodeCombResources/Test/archive/master.zip","c:\\test\\package\\").Wait();
             Console.Read();
         }
